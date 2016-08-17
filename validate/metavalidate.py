0bcede7a0ae55823e51511afbaf8a4c6a8d8af1f
m-schu <mschu_10@ulbnet.uni-muenster.de>
2016-08-17 09:44:09 +0200

    replace nonascii for py2.7

--- validate/metavalidate.py
+++ validate/metavalidate.py
@@ -1,5 +1,5 @@
 '''
-    Copyright © 2016 - o2r project
+    Copyright (c) 2016 - o2r project
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
